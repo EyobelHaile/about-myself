34c96e0593c5d0c62254ac23213ccb26cd69d979
Eyobel Haile <eyoh360@gmail.com>
2023-06-28 21:23:23 -0700

    updates index

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = "Bella";
+var lastName = "Haile";
+var yearOfStudy = "four months";
+var goal = "To become a jr Front-end Developer"
+var outputElement = document.getElementById("ouput");
+
+outputElement.textContent("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.");
+
+
